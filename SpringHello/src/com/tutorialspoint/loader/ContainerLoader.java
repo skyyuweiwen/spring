e90950b2ecb407e75c1993d7191fcbd90cbc95d3
Terry Tan <terry.tan01@sap.com>
2017-03-03 13:23:46 +0800

    1. add static to the ApplicationContext context

--- SpringHello/src/com/tutorialspoint/loader/ContainerLoader.java
+++ SpringHello/src/com/tutorialspoint/loader/ContainerLoader.java
@@ -4,7 +4,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ContainerLoader {
-	protected ApplicationContext context ;
+	protected static ApplicationContext context ;
 	
 	public ContainerLoader(){
 		context = new ClassPathXmlApplicationContext("Beans.xml");
