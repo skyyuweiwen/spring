41401f239ed507aa2019b988b7ca09c1a85c00e1
MSI-PC\Administrator <420766954@qq.com>
2017-03-04 22:40:26 +0800

    1. Practices for the different way DI.

---
+++ SpringHello/src/com/tutorialspoint/di/Di.java
@@ -0,0 +1,17 @@
+package com.tutorialspoint.di;
+
+import org.springframework.context.support.AbstractApplicationContext;
+
+public class Di {
+	public void showdi(AbstractApplicationContext context) {
+		context.getBean("spellChecker");
+		TextEditor te = (TextEditor) context.getBean("textEditor");
+		te.spellCheck();
+		
+		JavaCollection jc=(JavaCollection)context.getBean("javaCollection");
+		jc.getAddressList();
+		jc.getAddressMap();
+		jc.getAddressProp();
+		jc.getAddressSet();
+	}
+}
