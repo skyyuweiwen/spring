55623eeb78b090a27db9ee71d66e5259d24eff79
terry.tan <terry.tan01@sap.com>
2018-10-15 08:43:29 -0700

    add the thymeleaf/freemarker

--- restart/src/main/java/com/example/restart/controller/HelloWorld.java
+++ restart/src/main/java/com/example/restart/controller/HelloWorld.java
@@ -1,13 +1,23 @@
 package com.example.restart.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 public class HelloWorld {
 
-    @RequestMapping("/")
+    @RequestMapping("/hello")
+    @ResponseBody
     public String hi(){
         return "Hello World";
     }
+
+    @RequestMapping(value="/")
+    public String index(ModelMap map){
+        map.addAttribute("host", "springboot-lyd");
+        return "index";
+    }
+
 }
